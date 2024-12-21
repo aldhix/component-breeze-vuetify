8a3395ffe35de082d5371c4e70fd21414702bc4a
Aldhi Xar <aldhi.x@gmail.com>
2024-12-21 21:37:30 +0700

    welcome

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,16 +6,32 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        const vueApp = createApp({ render: () => h(App, props) })
+            .use(vuetify)
             .use(plugin)
             .use(ZiggyVue)
-            .mount(el);
+            
+        vueApp.config.globalProperties.$appName = appName
+        vueApp.mount(el);
+
+        return vueApp;
     },
     progress: {
         color: '#4B5563',
